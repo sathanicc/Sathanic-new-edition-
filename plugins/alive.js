43a36e24d09f3cb26064e20c1888b999b0438acd
〘 sᴀᴛʜᴀɴɪᴄ 〙 <k75384759@gmail.com>
2025-03-22 01:45:32 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -1,13 +1,22 @@
-const { bot, aliveMessage, lang } = require('../lib/')
+const { bot, aliveMessage, lang } = require('../lib/');
 
 bot(
   {
-    pattern: 'alive ?(.*)',
-    desc: lang.plugins.alive.desc,
-    type: 'misc',
+    pattern: 'alive ?(.*)', // Command pattern to trigger the alive message
+    desc: lang.plugins.alive.desc, // Description of the command
+    type: 'misc', // Command type
   },
   async (message, match) => {
-    const { msg, options, type } = await aliveMessage(match, message)
-    return await message.send(msg, options, type)
+    try {
+      // Retrieve the alive message details asynchronously
+      const { msg, options, type } = await aliveMessage(match, message);
+      
+      // Send the response message with the appropriate options and type
+      return await message.send(msg, options, type);
+    } catch (error) {
+      // Handle any errors that may occur during processing
+      console.error('Error while processing alive command:', error);
+      return await message.send('An error occurred while retrieving the alive message. Please try again.');
+    }
   }
-)
+);
